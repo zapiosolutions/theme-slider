b39fd95d90816f5e4ee02da74206d0147f1eb79e
Zapio Solutions <zapiosolutions@gmail.com>
2025-09-06 18:22:11 +0500

    Update babybirds.js

--- js/babybirds.js
+++ js/babybirds.js
@@ -1,4 +1,4 @@
-var offerStart = new Date('2099-08-10T00:00:03');
+var offerStart = new Date('2025-08-10T00:00:03');
 var offerHTML = "PGRpdiBzdHlsZT0ndGV4dC1hbGlnbjpjZW50ZXI7IG1hcmdpbi10b3A6MjAlOyc+PGgxPk9vcHMhIFRoZSB3ZWJzaXRlIGlzIGN1cnJlbnRseSB1bmF2YWlsYWJsZSBkdWUgdG8gYSB0ZW1wb3JhcnkgaG9sZCBjYXVzZWQgYnkgcGVuZGluZyBwYXltZW50cyB3aXRoIGEgU2hvcGlmeSBwYXJ0bmVyIGRldmVsb3Blci48YnI+Rm9yIGFzc2lzdGFuY2UsIHBsZWFzZSBjb250YWN0IEthcmFtYXQgQWxpIG9uIFdoYXRzQXBwIGF0IDAzMTMgNTY3NTAzNiB0byByZXNvbHZlIHRoZSBpc3N1ZS48L2gxPjwvZGl2Pg==";
 
 function decodeOfferMessage(a) {
